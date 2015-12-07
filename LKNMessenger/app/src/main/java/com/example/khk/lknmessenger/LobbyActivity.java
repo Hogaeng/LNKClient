5f81035cae1a1b85ae03a5d0ff97e6437e0eb5ce
revoice-tial <alinelice@naver.com>
2015-12-08 01:16:33 +0900

    Lobby activity

--- LKNMessenger/app/src/main/java/com/example/khk/lknmessenger/LobbyActivity.java
+++ LKNMessenger/app/src/main/java/com/example/khk/lknmessenger/LobbyActivity.java
@@ -2,6 +2,7 @@ package com.example.khk.lknmessenger;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.TabActivity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Gravity;
@@ -14,42 +15,19 @@ import android.widget.TextView;
 /**
  * Created by KHK on 2015-12-07.
  */
-public class LobbyActivity extends FragmentActivity{
-    TabHost tabHost;
-    public void onCreate(Bundle savedInstanceState)
-    {
+public class LobbyActivity extends TabActivity {
+    private TabHost mTabHost;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lobbywindow);
 
-        tabHost = (TabHost) findViewById(android.R.id.tabhost);
-        tabHost.setup();
+        mTabHost = getTabHost();
 
-        TabHost.TabSpec spec1 = tabHost.newTabSpec("Tab1").setContent(R.id.Friend).setIndicator(getString(R.string.tab1));
-        tabHost.addTab(spec1);
+        // 탭 1, 2, 3 을 추가하면서 태그를 달아주고, 제목(또는 아이콘)을 설정한다.
+        mTabHost.addTab(mTabHost.newTabSpec("Friend").setContent(R.id.tabs1).setIndicator("Friend"));
+        mTabHost.addTab(mTabHost.newTabSpec("Talk").setContent(R.id.tabs2).setIndicator("Talk"));
 
-        TabHost.TabSpec spec2 = tabHost.newTabSpec("Tab2").setContent(R.id.Talk).setIndicator(getString(R.string.tab2));
-        tabHost.addTab(spec2);
-
-        tabHost.getTabWidget().getChildAt(0).getLayoutParams().height = 60;
-        tabHost.getTabWidget().getChildAt(1).getLayoutParams().height = 60;
-
-        LinearLayout.LayoutParams tvParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT);
-
-        RelativeLayout r11 = (RelativeLayout) tabHost.getTabWidget().getChildAt(0);
-        r11.setGravity(Gravity.CENTER_VERTICAL);
-        TextView tv1 = (TextView) r11.getChildAt(1);
-        tv1.setLayoutParams(tvParams);
-        tv1.setTextAppearance(this, android.R.style.TextAppearance_Medium);
-        tv1.setPadding(10, 0, 10, 0);
-        tv1.setGravity(Gravity.CENTER);
-
-        RelativeLayout r12 = (RelativeLayout) tabHost.getTabWidget().getChildAt(1);
-        r12.setGravity(Gravity.CENTER_VERTICAL);
-        TextView tv2 = (TextView) r11.getChildAt(1);
-        tv2.setLayoutParams(tvParams);
-        tv2.setTextAppearance(this, android.R.style.TextAppearance_Medium);
-        tv2.setPadding(10, 0, 10, 0);
-        tv2.setGravity(Gravity.CENTER);
     }
 }
