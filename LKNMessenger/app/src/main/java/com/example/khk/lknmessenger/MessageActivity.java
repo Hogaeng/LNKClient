eb99a9ee1dbc92e89479bf804bdfe2fefbc629bc
호준 <jeff8080@naver.com>
2015-12-10 20:48:03 +0900

    comiit

--- LKNMessenger/app/src/main/java/com/example/khk/lknmessenger/MessageActivity.java
+++ LKNMessenger/app/src/main/java/com/example/khk/lknmessenger/MessageActivity.java
@@ -77,11 +77,56 @@ public class MessageActivity extends Activity {
 
                 {
                     sendMsg = PacketCodec.encodeMssReq(mssReq);
+                    Log.e("sendMsg", sendMsg);
+
                     try {
+                        Log.e("2sendMsg", "Link Start!!"); //Toast.makeText(getBaseContext(),"Link Start!!",Toast.LENGTH_SHORT).show();
                         SocketManager.sendMsg(sendMsg);
+                        Log.e("3sendMsg", sendMsg);
+
+                        recvMsg = SocketManager.receiveMsg();
+                        Log.e("recvMsg", recvMsg);
+                        packet = PacketCodec.decodeHeader(recvMsg);
                     } catch (IOException e) {
                         e.printStackTrace();
+
                     }
+
+                    mssAck = PacketCodec.decodeMssAck(packet.getData());
+
+                    //  analyzing the ACK sent from server
+                    if (mssAck.getAnswer() == Packet.FAIL) {
+                        AlertDialog.Builder dialog = new AlertDialog.Builder(MessageActivity.this);
+                        dialog.setMessage("Please Input Correct Sentence");
+                        Log.e("Please","Please Input Correct Sentence");
+                        dialog.setPositiveButton("ok", null);
+                        Log.e("ok","ok");
+                        dialog.show();
+                    }                   //  if the ACK means login fail, create the alert dialog
+
+                    else if (mssAck.getAnswer() == Packet.SUCCESS) {
+                        arrAdapter.add(message);
+                        message = "";
+                        editText.setText("");
+                        String[] arg;
+                        MssAndArrtimeAndUser[] mau;
+                        if (mssAck.getListnum() > 0) {
+                            arg = PacketCodec.nextDecode(mssAck.getListnum(), mssAck.getlist());
+                            Log.e("arg[0]",arg[0]);
+                            mau = PacketCodec.nextTinydecode(arg.length, arg);
+                            for (int i = 0; i < mau.length; i++) {
+                                Log.e("Text", mau[i].getMss());
+                                arrAdapter.add(mau[i].getMss());
+                            }
+
+                        } else {
+                            Toast.makeText(getBaseContext(), "Send!", Toast.LENGTH_SHORT).show();
+                            Log.e("Sever", "Send!");
+                        }
+
+                    }
+
+
                 }
             }
         };
